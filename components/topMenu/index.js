08d8d69370512fa43344d028492ce631f72383db
fozg <phongdaohong@gmail.com>
2018-11-18 09:06:28 +0700

    Add Topmenu

---
+++ components/topMenu/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default class TopMenu extends React.Component{
+  state = {
+    isActive: false
+  }
+
+  componentDidMount () {
+    document.addEventListener('keypress', this.listenEventKeyPress);
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('keypress', this.listenEventKeyPress);
+  }
+
+  listenEventKeyPress = (e) => {
+    if (e.key === '`') {
+      this.setState({isActive: !this.state.isActive});
+    }
+  }
+
+  render () {
+    const {
+      isActive
+    } = this.state;
+    
+    return (
+      <div id="menu" className={`TopMenu row m-0 align-items-center ${isActive &&'TopMenu__active'}`}>
+        <div className="p-2">
+          <img src="static/images/logo.png" alt="fozg.net" width="35"/>
+        </div>
+        <div className="col d-flex justify-content-end m-2">
+          <div className="row">
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Page from '../components/page';
+import TopMenu from '../components/topMenu';
 import Intro from '../components/intro'
 import AppList from '../components/appList';
 
 export default () => (
   <Page title="Fozg.net">
+    <TopMenu />
     <Intro />
     
     <div className="col-md-8 col-sm-10 mx-auto">
